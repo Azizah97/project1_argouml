26021577bb1481c5acb8d02bc0a6833cf151f7b2
Azizah97 <nurazizah.ekabudiarti@gmail.com>
2017-05-31 05:57:03 +0700

    project1 versi 1

---
+++ class.Student.php
@@ -0,0 +1,99 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * untitledModel - class.Student.php
+ *
+ * $Id$
+ *
+ * This file is part of untitledModel.
+ *
+ * Automatically generated on 26.04.2017, 07:26:35 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * include Teacher
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ */
+require_once('class.Teacher.php');
+
+/* user defined includes */
+// section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000877-includes begin
+// section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000877-includes end
+
+/* user defined constants */
+// section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000877-constants begin
+// section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000877-constants end
+
+/**
+ * Short description of class Student
+ *
+ * @access public
+ * @author firstname and lastname of author, <author@example.org>
+ */
+class Student
+{
+    // --- ASSOCIATIONS ---
+    // generateAssociationEnd : 
+
+    // --- ATTRIBUTES ---
+
+    /**
+     * Short description of attribute Student
+     *
+     * @access private
+     * @var Integer
+     */
+    private $Student = null;
+
+    /**
+     * Short description of attribute Name
+     *
+     * @access public
+     * @var String
+     */
+    public $Name = null;
+
+    /**
+     * Short description of attribute Address
+     *
+     * @access public
+     * @var String
+     */
+    public $Address = null;
+
+    /**
+     * Short description of attribute City
+     *
+     * @access public
+     * @var String
+     */
+    public $City = null;
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method AddStudent
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @return mixed
+     */
+    public function AddStudent()
+    {
+        // section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000884 begin
+        // section 127-0-0-1--51d35dc7:15ba7839dcc:-8000:0000000000000884 end
+    }
+
+} /* end of class Student */
+
+?>
